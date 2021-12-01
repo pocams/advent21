ccf44cccf531e665bfcc80a2800d04447333499c
Mark Johnston <git@mhj.ca>
2021-12-01 13:22:58 -0600

    Day 1. The .tuple_windows().tuple_windows() construction could definitely use some type hints, but I kind of get a kick out it the way it is.
    Also add a bit of scaffolding for reading input files.

---
+++ src/day1.rs
@@ -0,0 +1,24 @@
+use color_eyre::Report;
+use itertools::Itertools;
+use tracing::info;
+
+pub(crate) fn part1(lines: &[String]) -> Result<(), Report> {
+
+    let numbers: Vec<u32> = lines.iter().map(|l| l.parse::<u32>()).collect::<Result<Vec<_>, _>>()?;
+    let part1 = numbers.iter()
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count();
+
+    info!(day=1, part=1, answer=part1);
+
+    let part2 = numbers.iter()
+        .tuple_windows()
+        .tuple_windows()
+        .filter(|((&a1, &b1, &c1), (&a2, &b2, &c2))| a1 + b1 + c1 < a2 + b2 + c2)
+        .count();
+
+    info!(day=1, part=2, answer=part2);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::path::PathBuf;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
 use color_eyre::Report;
 use structopt::StructOpt;
+use tracing::debug;
 use tracing_subscriber::EnvFilter;
+
+mod day1;
 
 #[derive(Debug, StructOpt)]
 #[structopt()]
@@ -35,8 +40,25 @@ fn set_up_logging(debug: bool) -> Result<(), Report> {
     Ok(())
 }
 
+fn read_lines(filename: &Path) -> Result<Vec<String>, Report> {
+    Ok(BufReader::new(File::open(filename)?)
+        .lines()
+        .collect::<Result<Vec<String>, _>>()?
+    )
+}
+
 fn main() -> Result<(), Report> {
     let options = Options::from_args();
     set_up_logging(options.debug)?;
+
+    let lines = read_lines(&options.input)?;
+
+    debug!("{file:?}: read {count} lines", file=options.input, count=lines.len());
+
+    match options.puzzle {
+        1 => day1::part1(&lines)?,
+        _ => panic!("No such puzzle: {day}", day=options.puzzle)
+    }
+
     Ok(())
 }
